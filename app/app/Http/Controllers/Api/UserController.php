cce0c79ca9ddb955fdaaabbca7d7f538937fc578
Reza Khalafiniya <khalafiniya@fr-k.de>
2022-05-10 13:52:49 +0200

    used Response constant codes instead of hardcoded code

--- app/app/Http/Controllers/Api/UserController.php
+++ app/app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -13,18 +14,21 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-        if ($validator->fails()){
-            return \response($validator->errors());
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]
+        );
+        if ($validator->fails()) {
+            return \response($validator->errors(), Response::HTTP_BAD_REQUEST);
         }
 
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            return response(['credential check failed'],401);
+            return response(['credential check failed'], Response::HTTP_UNAUTHORIZED);
         }
 
         return $user->createToken($user->email)->plainTextToken;
@@ -32,13 +36,16 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'email' => 'required|email',
-            'password' => 'required',
-            'name' => 'required',
-        ]);
-        if ($validator->fails()){
-            return \response($validator->errors());
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required',
+                'name' => 'required',
+            ]
+        );
+        if ($validator->fails()) {
+            return \response($validator->errors(), Response::HTTP_BAD_REQUEST);
         }
         $user = new User();
         $user->name = $request->name;
