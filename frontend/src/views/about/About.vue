674e620c327077e9dc1cd6c0b71970bd802dd926
Reza Khalafiniya <khalafiniya@fr-k.de>
2022-05-11 09:10:29 +0200

    init vue2 app with seperated js scss and vue files

---
+++ frontend/src/views/about/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+    </div>
+</template>
+<style src="./about.scss" lang="scss" scoped></style>
+<script src="./about.js"></script>
