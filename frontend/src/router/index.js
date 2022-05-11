5f4b0b7bc19aabfb86775710016a9e93ce71892a
Reza Khalafiniya <khalafiniya@fr-k.de>
2022-05-11 10:38:26 +0200

    changed base router style link

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -25,6 +25,8 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  linkActiveClass: "active",
+  linkExactActiveClass: "exact-active",
 });
 
 export default router;
