047ce0ff2fade8a88d4eb4b4e5d99e8f58665d18
Reza Khalafiniya <khalafiniya@fr-k.de>
2022-05-11 13:10:10 +0200

    moved bootstrapVue to plugins folder

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,17 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './main.scss'
-import { LayoutPlugin } from 'bootstrap-vue'
-Vue.use(LayoutPlugin)
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+import './plugins/bootstrapVue';
 
 Vue.config.productionTip = false;
 
---
+++ frontend/src/plugins/bootstrapVue.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '../main.scss'
+import { LayoutPlugin } from 'bootstrap-vue'
+Vue.use(LayoutPlugin)
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
